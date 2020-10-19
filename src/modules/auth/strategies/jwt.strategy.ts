83bf025e3144183b386e0b085888e58c72df0bcb
AbibF <gonzalo.moreno@televisatim.com>
2020-10-19 11:54:45 -0500

    FIX: where username => Se necesita un nombre para la estrategia

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,29 +1,35 @@
-import { IJwtPayload } from './../jwt-payload.interface';
-import { AuthRepository } from './../auth.repository';
-import { Configuration } from './../../../config/config.keys';
-import { ConfigService } from './../../../config/config.service';
-import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport";
-import { ExtractJwt } from "passport-jwt";
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { ConfigService } from '../../../config/config.service';
+import { Configuration } from '../../../config/config.keys';
+import { AuthRepository } from '../auth.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IJwtPayload } from '../jwt-payload.interface';
+import { UnauthorizedException, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy){
-    constructor(private readonly _configService: ConfigService, private readonly _authRepository: AuthRepository){
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: _configService.get(Configuration.JWT_SECRET),
-        });
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly _configService: ConfigService,
+    @InjectRepository(AuthRepository)
+    private readonly _authRepository: AuthRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: _configService.get(Configuration.JWT_SECRET),
+    });
+  }
+
+  async validate(payload: IJwtPayload) {
+    const { username } = payload;
+    const user = await this._authRepository.findOne({
+      where: { username, status: 'ACTIVE' },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException();
     }
 
-    async validate(payload: IJwtPayload) {
-        const { username } = payload;
-        const user = await this._authRepository.findOne(username, {where: { status: "ACTIVE"}});
-
-        if(!user) {
-            throw new UnauthorizedException();
-        }
-
-        return payload;
-    }
+    return payload;
+  }
 }
